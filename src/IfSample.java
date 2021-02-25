f9088aa3eb28af1a2442b5425108b2aeb37274da
Paul <hughpaud@gmail.com>
2021-02-25 17:29:20 -0600

    :construction: Created IfSample.java

---
+++ src/IfSample.java
@@ -0,0 +1,15 @@
+/*
+    Demonstrate the if statement.
+    Call the file "IfSample.java"
+
+*/
+
+
+
+
+public class IfSample
+ {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
