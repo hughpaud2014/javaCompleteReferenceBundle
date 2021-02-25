b22be49c105b890fa187d71af0e1f8c2fc3ea5b8
Paul <hughpaud@gmail.com>
2021-02-25 16:36:30 -0600

    :construction: Updated SecondSimpleJavaApp.java

--- src/SecondSimpleJavaApp.java
+++ src/SecondSimpleJavaApp.java
@@ -6,6 +6,7 @@
 
 public class SecondSimpleJavaApp {
     public static void main(String[] args) throws Exception {
+        
         int num; // This declares a variable called num, you will see in other examples, there is other ways of declaring variables.
         num = 100;
         System.out.println("This is num: " + num);
