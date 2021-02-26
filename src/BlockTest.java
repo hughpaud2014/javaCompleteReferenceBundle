1e6ed51137ce4d63fc0e855bec1e886937c14be9
Paul <hughpaud@gmail.com>
2021-02-26 13:51:33 -0600

    :books: Created BlockTest.java

---
+++ src/BlockTest.java
@@ -0,0 +1,19 @@
+/*
+    Demonstrates a block of code
+    Call this file "BlockTest.java"
+
+*/
+
+public class BlockTest {
+    public static void main(String args[]) {
+        int x, y;
+        y = 20;
+
+        // The target of this loop is a block.
+        for (x = 0; x < 10; x++) {
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+            y = y - 2;
+        }
+    }
+}
