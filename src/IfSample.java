f847e550ef2bae44fb11f7e2844565eae77cb8ff
Paul <hughpaud@gmail.com>
2021-02-26 12:23:13 -0600

    :books: Updated IfSample.java

--- src/IfSample.java
+++ src/IfSample.java
@@ -4,12 +4,33 @@
 
 */
 
-
-
-
-public class IfSample
- {
+public class IfSample {
     public static void main(String[] args) {
-        System.out.println();
+
+        // Establish variable types
+        int x, y;
+
+        // Define variable values
+        x = 10;
+        y = 20;
+
+        // Declare if statements
+        if (x < y)
+            System.out.println("x is less than y");
+
+        x = x * 2;
+
+        if (x == y)
+            System.out.println("x now is equal to y");
+
+        x = x * 2;
+
+        if (x > y)
+            System.out.println("x now greater than y");
+
+        // This will not display anything
+        if (x == y)
+            System.out.println("You won't see this.");
+
     }
 }
