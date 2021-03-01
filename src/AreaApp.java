e0c538bfde8c682f777d2cfae6e0151b747215fa
Paul <hughpaud@gmail.com>
2021-03-01 16:05:32 -0600

    :construction: Created AreaApp.java

---
+++ src/AreaApp.java
@@ -0,0 +1,19 @@
+// Compute Area of a circle
+// Call file "AreaApp.java"
+
+
+public class AreaApp {
+    public static void main(String args[]) {
+        double pi, r, a;
+
+        r = 10.8; // radius of a circle
+        pi = 3.1416; // pi, approximately
+        a = pi * r * r;
+
+        System.out.println("Area of a circle is " + a);
+
+
+    }
+
+    
+}
