5b30db9a166e55dbd3ad9ce1ddcbfdccbf006809
Paul Hughes <hughpaud@gmail.com>
2021-03-03 21:34:33 -0600

    :construction: Hello world!

---
+++ src/Track1/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package Track1.HelloWorld;
+
+// Call this file "HelloWorld.java"
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+    
+}
