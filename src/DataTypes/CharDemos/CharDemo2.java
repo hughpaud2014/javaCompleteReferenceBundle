d49c1b0f6e9f441af7e8b941b95e03e9c28f4a5a
Paul Hughes <hughpaud@gmail.com>
2021-03-05 14:18:54 -0600

    :construction: CharDemo2.java

---
+++ src/DataTypes/CharDemos/CharDemo2.java
@@ -0,0 +1,22 @@
+package DataTypes.CharDemos;
+/*
+    char variables behave like integers.
+    Call this file "CharDemo2.java"
+
+*/
+
+
+
+public class CharDemo2 {
+    public static void main(String args[]) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains: " + ch1);
+
+        ch1++; // Increment ch1
+        System.out.println("ch1 is now: " + ch1);
+
+    }
+
+}
